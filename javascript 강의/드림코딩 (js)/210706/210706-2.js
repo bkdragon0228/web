5d4a4d51645620241ff68046c828b73410e08466
bkdragon0228 <rlaqjarb2067@naver.com>
2021-07-09 23:45:09 +0900

    html,css_정리_

--- javascript 강의/드림코딩 (js)/210706/210706-2.js
+++ javascript 강의/드림코딩 (js)/210706/210706-2.js
@@ -27,3 +27,29 @@ fruits.forEach((v, i) => {
 // shift, unshift는 pop, push에 비해 엄청 느리다.
 
 // shift, unshift는 pop, push에 비해 엄청 느리다.
+
+//splice
+// 시작(인덱스값)부터 몇개나 지울 것인지
+// 몇개인지를 입력 안하면 그 인덱스부터 다 지움
+fruits.push('lemon', 'peach', 'berry');
+fruits.splice(1, 1);
+console.log(fruits);
+fruits.splice(0, 2, 'watermelon', 'apple'); // 지우고 그자리에 추가!
+console.log(fruits);
+
+//combine two arrays
+const fruits2 = ['sweetpotato'];
+const newFruits = fruits.concat(fruits2);
+console.log(newFruits);
+
+console.clear();
+// searching
+console.log(fruits.indexOf('apple')); // 1
+// 인덱스값을 리턴, 없을 떄 -1을 리턴
+console.log(fruits.includes('coconat'));
+// 포함하는지 true or false
+
+//lastindexof
+fruits.push('apple');
+console.log(fruits.indexOf('apple')); //1
+console.log(fruits.lastIndexOf('apple')); //4
