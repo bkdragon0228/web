a429dd4def71489b8efd816c5b8ab174ac2e5dbb
bkdragon0228 <rlaqjarb2067@naver.com>
2021-08-03 16:46:42 +0900

    신호등

---
+++ CSS animation/신호등/app.js
@@ -0,0 +1,17 @@
+const circles = document.querySelectorAll('.circle');
+let activeLight = 0;
+
+function changeLight() {
+    circles[activeLight].className = 'circle';
+    // 색을 초기화
+    activeLight++;
+    if (activeLight > 2) {
+        activeLight = 0;
+        //다시 빨강으로 돌아가기
+    }
+    const currentLight = circles[activeLight];
+    currentLight.classList.add(currentLight.getAttribute('color'));
+    // 어트리뷰트에 준 color를 이용!
+}
+
+setInterval(changeLight, 2000);
